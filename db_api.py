25e8e8e497a28d96bc74e8f4a523048c9a9b55e9
666Darkness <rdvolti@gmail.com>
2024-07-07 15:08:03 +0200

    V End

--- db_api.py
+++ db_api.py
@@ -398,8 +398,270 @@ if __name__ == "__main__":
                   """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/skywrath_mage_mystic_flare.png"""
                 ,5)
         
-
-
+        add_hero("AXE",
+                """Axe рубит одного врага за другим, неизменно ступая впереди своей команды. Он вынуждает противников вступить в бой, а затем отвечает на их удары смертоносными взмахами топора. Нещадно круша ослабленных врагов, он всегда несётся вперёд.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/axe.png""",
+                25,
+                20,
+                18,
+                670,
+                291,
+                """55-59""",
+                3.3,
+                315,
+                150,
+                1)
+        add_spell("BERSERKER'S CALL",
+                  """Герой бросает вызов ближайшим врагам, заставляя их атаковать его, а также получает бонус к броне на время действия способности.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/axe_berserkers_call.png""",
+                6)
         
+        add_spell("BATTLE HUNGER",
+                  """Приводит врага в бешенство, нанося ему урон, пока он кого-нибудь не убьёт или действие способности не закончится. Урон от способности увеличивается в зависимости от брони владельца способности. Скорость передвижения жертвы уменьшается, когда она не смотрит в сторону владельца способности.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/axe_battle_hunger.png"""
+                ,6)
+
+        add_spell("COUNTER HELIX",
+                  """После определённого числа полученных атак герой прокручивает вокруг себя топор, нанося чистый урон всем врагам неподалёку.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/axe_counter_helix.png"""
+                ,6)
+
+        add_spell("CULLING BLADE",
+                  """Герой находит слабую точку врага и наносит ему чистый урон. Если убить этой способностью вражеского героя, её перезарядка сбрасывается, а все союзники поблизости временно получат дополнительную скорость передвижения и броню.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/axe_culling_blade.png"""
+                ,6)
+        
+        add_hero("HUSKAR",
+                """Чем меньше у Huskar здоровья, тем опаснее он становится. Своим ультом он мгновенно лишает себя и противника части здоровья, а на грани смерти может обрушить на своих врагов шквал горящих копий.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/huskar.png""",
+                23,
+                10,
+                18,
+                626,
+                216,
+                """44-49""",
+                2.7,
+                295,
+                400,
+                1)
+        add_spell("INNER FIRE",
+                  """Огненная ярость героя отталкивает всех врагов неподалёку на определённое расстояние от себя, наносит им урон и накладывает на них безмолвие.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/huskar_inner_fire.png""",
+                7)
+        
+        add_spell("BURNING SPEAR",
+                  """Герой жертвует частью здоровья, чтобы поджечь своё копьё и добавить к обычной атаке складывающийся периодический урон.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/huskar_burning_spear.png"""
+                ,7)
+
+        add_spell("BERSERKER'S BLOOD",
+                  """Раны делают героя сильнее, увеличивая его скорость атаки, сопротивление магии и восстановление здоровья в зависимости от доли недостающего здоровья. Восстановление равно доле от силы героя.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/huskar_berserkers_blood.png"""
+                ,7)
+
+        add_spell("LIFE BREAK",
+                  """Герой наносит огромный урон ценой собственного здоровья. Он прыгает на жертву, отнимая часть её текущего здоровья и замедляя её атаку и передвижение. Во время прыжка герой невосприимчив к эффектам и получает +60% к сопротивлению магии.
+
+ТИП РАЗВЕИВАНИЯ: нормальное""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/huskar_life_break.png"""
+                ,7)
+        
+        add_hero("PHANTOM ASSASSIN",
+                """Phantom Assassin идёт в наступление, едва завидев свою жертву. Мгновенно сблизившись с ней, Мортред легко уклоняется от атак и обрушивает на врага удар за ударом, любой из которых может оказаться роковым.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/phantom_assassin.png""",
+                19,
+                21,
+                15,
+                538,
+                255,
+                """56-58""",
+                4.5,
+                310,
+                150,
+                2)
+        add_spell("STIFLING DAGGER",
+                  """Бросает кинжал, который замедляет скорость передвижения врага, наносит ему физический урон в размере 65 + 30% от атаки героя, а также накладывает эффекты предметов и способностей.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/phantom_assassin_stifling_dagger.png""",
+                8)
+        
+        add_spell("PHANTOM STRIKE",
+                  """Герой телепортируется к выбранному существу и, если это противник, получает дополнительную скорость атаки.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/phantom_assassin_phantom_strike.png"""
+                ,8)
+
+        add_spell("BLUR",
+                  """Герой размывает свои очертания, становясь невидимым и передвигаясь быстрее, пока не совершит атаку или не приблизится к вражескому герою, башне или аванпосту.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/phantom_assassin_blur.png"""
+                ,8)
+
+        add_spell("COUP DE GRACE",
+                  """Герой оттачивает свои боевые навыки, с каждой атакой имея шанс войти в состояние смертельного фокуса. В нём следующая атака гарантированно нанесёт критический урон и снимет фокус. Способность Stifling Dagger даёт фокус с повышенной вероятностью.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/phantom_assassin_coup_de_grace.png"""
+                ,8)
+        
+        add_hero("BLOODSEEKER",
+                """Bloodseeker постоянно ставит противников перед непростым выбором. Окропив кровью обширную территорию, он вынуждает врагов отступать, а под его чудовищным ультом жертва либо стоит на месте, либо погибает.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/bloodseeker.png""",
+                24,
+                24,
+                17,
+                648,
+                279,
+                """59-65""",
+                6.0,
+                285,
+                150,
+                2)
+        add_spell("BLOODRAGE",
+                  """Пробуждает в существе жажду крови, из-за которой оно атакует быстрее и наносит больше урона заклинаниями, но каждую секунду теряет долю от своего здоровья. Даёт союзным героям половину от скорости атаки.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/bloodseeker_bloodrage.png""",
+                9)
+        
+        add_spell("BLOOD RITE",
+                  """Герой окропляет выбранную область святой кровью. Через 2,9 сек. ритуал завершается, нанося противникам урон и запрещая им применять способности.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/bloodseeker_blood_bath.png"""
+                ,9)
+
+        add_spell("THIRST",
+                  """Герой упивается ранениями противников, отчего его передвижение ускоряется, когда здоровье любого вражеского героя становится меньше 100%. Чем ниже уровень здоровья, тем сильнее эффект. Если здоровье вражеского героя упадёт ниже 25%, его станет видно в любой точке карты, даже если он невидим. Бонусы от разных противников складываются и позволяют превысить максимальную скорость передвижения.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/bloodseeker_thirst.png"""
+                ,9)
+
+        add_spell("RUPTURE",
+                  """Разрывает кожу врага, нанося жертве начальный урон, зависящий от её текущего здоровья. Если цель передвигается, то она получает урон, зависящий от преодолённого расстояния.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/bloodseeker_rupture.png"""
+                ,9)
+
+        add_hero("LION",
+                """Lion не даёт врагам выбраться из своей цепкой хватки. Он оглушает жертву каменными шипами и на время превращает её в безобидную зверушку. Если одних его заклинаний не хватит, чтобы расправиться с врагом, он обязательно даст команде достаточно времени.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/lion.png""",
+                18,
+                15,
+                20,
+                516,
+                315,
+                """49-55""",
+                2.5,
+                290,
+                600,
+                3)
+        add_spell("EARTH SPIKE",
+                  """Из земли прорывается полоса каменных шипов. Они подбрасывают врагов в воздух, а по приземлении оглушают их и наносят урон.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lion_impale.png""",
+                10)
+        
+        add_spell("HEX",
+                  """Превращает врага в безобидную зверюшку, блокируя все его способности.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lion_voodoo.png"""
+                ,10)
+
+        add_spell("MANA DRAIN",
+                  """ПРЕРЫВАЕМАЯ — сосредотачивает свою магическую энергию, поглощая ману жертвы и замедляя её передвижение. Можно применять на союзника, чтобы передавать ему свою ману и ускорить его передвижение на 50%.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lion_mana_drain.png"""
+                ,10)
+
+        add_spell("FINGER OF DEATH",
+                  """Разрывает вражеское существо, пытаясь вывернуть его наизнанку. Наносит большой урон, который увеличивается с каждым убитым этой способностью врагом.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lion_finger_of_death.png"""
+                ,10)
+
+        add_hero("LINA",
+                """Опасная и хрупкая Lina легко свалит любого одинокого врага. Она поражает противников огнём и молнией, а каждое произнесённое ей заклинание увеличивает скорость её атаки, не давая выжить никому.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/lina.png""",
+                20,
+                23,
+                30,
+                560,
+                435,
+                """51-59""",
+                3.8,
+                290,
+                670,
+                3)
+        add_spell("DRAGON SLAVE",
+                  """Герой высвобождает дыхание дракона, сжигающее всех врагов на своём пути.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lina_dragon_slave.png""",
+                11)
+        
+        add_spell("LIGHT STRIKE ARRAY",
+                  """Призывает столб пламени, который оглушает врагов и наносит им урон.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lina_light_strike_array.png"""
+                ,11)
+
+        add_spell("FIERY SOUL",
+                  """Увеличивает скорость атаки и передвижения всякий раз, когда герой попадает способностью по врагу. Эффекты способности складываются друг с другом. Действует 18 сек.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lina_fiery_soul.png"""
+                ,11)
+
+        add_spell("LAGUNA BLADE",
+                  """Выпускает разряд молнии в одного врага, нанося сокрушительный урон.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/lina_laguna_blade.png"""
+                ,11)
+        
+        add_hero("DAZZLE",
+                """Рождённый поддерживать своих сторонников, Dazzle не даёт соратникам умереть, пока те уничтожают врагов. Его необычные заклинания вплетаются в броню, ослабляя противников и усиливая союзников.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/dazzle.png""",
+                18,
+                20,
+                25,
+                516,
+                375,
+                """47-53""",
+                3.3,
+                305,
+                575,
+                4)
+        add_spell("POISON TOUCH",
+                  """Выпускает конус яда, поражающий нескольких врагов. Наносит жертвам периодический урон и замедляет их, а атаки владельца способности обновляют время действия эффекта и усиливают замедление.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/dazzle_poison_touch.png""",
+                12)
+        
+        add_spell("SHALLOW GRAVE",
+                  """Накладывает на союзника священную защиту, не позволяющую ему умереть. Также увеличивает его лечение в зависимости от отсутствующего у него здоровья.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/dazzle_shallow_grave.png"""
+                ,12)
+
+        add_spell("SHADOW WAVE",
+                  """Выпускает мощный заряд, который передаётся от союзника к союзнику, излечивая их и нанося урон противникам неподалёку. Способность всегда лечит своего владельца.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/dazzle_shadow_wave.png"""
+                ,12)
+
+        add_spell("BAD JUJU",
+                  """Уменьшает перезарядки остальных способностей. Применение этой способности временно повышает расход здоровья на неё.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/dazzle_bad_juju.png"""
+                ,12)
+        
+        add_hero("VENOMANCER",
+                """Venomancer несёт своим врагам медленную смерть. Он взращивает бесконечную армию прыскающих ядом защитников, а стоит неприятелям показаться рядом, он выпускает огромное облако яда, надолго ослабляющего недругов.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/heroes/venomancer.png""",
+                19,
+                24,
+                19,
+                538,
+                303,
+                """42-45""",
+                4.0,
+                280,
+                450,
+                4)
+        add_spell("VENOMOUS GALE",
+                  """Выплёвывает в указанном направлении ядовитый шар, который наносит урон всем задетым врагам и отравляет их. Поражённые цели медленнее передвигаются, а также получают урон каждые 3 секунды действия способности.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/venomancer_venomous_gale.png""",
+                13)
+        
+        add_spell("PLAGUE WARD",
+                  """Призывает чумной тотем, атакующий вражеских существ и сооружения. Тотем невосприимчив к магии. Он имеет текущий уровень способности Poison Sting, нанося 50% от её урона.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/venomancer_plague_ward.png"""
+                ,13)
+
+        add_spell("POISON STING",
+                  """Добавляет в обычные атаки героя яд, который наносит периодический урон и снижает скорость передвижения жертвы.""",
+                  """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/venomancer_poison_sting.png"""
+                ,13)
+
+        add_spell("NOXIOUS PLAGUE",
+                  """Накладывает на врага смертоносную заразу, которая наносит начальный урон, а затем наносит урон в зависимости от максимального здоровья цели. Зараза замедляет жертву и врагов рядом с ней — чем они ближе, тем замедление сильнее. Когда жертва умирает или действие способности заканчивается, все враги неподалёку получают такой же эффект заразы, который не передаётся другим существам.""",
+                """https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/abilities/venomancer_noxious_plague.png"""
+                ,13)
 
 
